c737807ea60923393a7a01da5c85b282d856d45f
Ahmed-Eltijani <ahmedsolokab@gmail.com>
2023-09-13 16:35:56 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,17 +11,19 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (mult = 1; mult <=9; mult++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putcahr(',');
 			_putchar(' ');
+
 			prod = num * mult;
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putcahr((prod / 10) + 48);
 			_putchar((prod % 10) + 48);
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
